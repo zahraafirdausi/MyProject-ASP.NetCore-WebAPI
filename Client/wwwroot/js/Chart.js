6eab917444b4b69afa299981f51b8948964498b2
zahraafirdausi <zahra_firdausi@apps.ipb.ac.id>
2023-05-26 14:47:39 +0700

    Modify index dan Add new chart

--- Client/wwwroot/js/Chart.js
+++ Client/wwwroot/js/Chart.js
@@ -153,7 +153,9 @@
                 //        options: chartOptions
                 //    });
                 //});
-                Piechart()
+                Piechart();
+                stakedbar();
+                barchart();
             },
             error: function (error) {
                 console.log(error);
@@ -162,46 +164,7 @@
     })
     
 });
-    //--------------------
-    //-------------
-    //- PIE CHART -
-    //-------------
-
-    //-------------
-    // Get context with jQuery - using jQuery's .get() method.
-    //var donutData = {
-    //    labels: [
-    //        'Chrome',
-    //        'IE',
-    //        'FireFox',
-    //        'Safari',
-    //        'Opera',
-    //        'Navigator',
-    //    ],
-    //    datasets: [
-    //        {
-    //            data: [700, 500, 400, 600, 300, 100],
-    //            backgroundColor: ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de'],
-    //        }
-    //    ]
-    //}
-
-    //var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
-    //var pieData = donutData;
-    //var pieOptions = {
-    //    maintainAspectRatio: false,
-    //    responsive: true,
-    //}
-    ////Create pie or douhnut chart
-    //// You can switch between pie and douhnut using the method below.
-    //new Chart(pieChartCanvas, {
-    //    type: 'pie',
-    //    data: pieData,
-    //    options: pieOptions
-    //})
-    //Piechart();
-
-//})
+    
 
 function Piechart() {
     $.ajax({
@@ -272,6 +235,306 @@ function Piechart() {
     });
 };
 
+function stakedbar() {
+    //---------------------
+    //- STACKED BAR CHART -
+    //---------------------
+    //var areaChartData = {
+    //    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    //    datasets: [
+    //        {
+    //            label: 'Digital Goods',
+    //            backgroundColor: 'rgba(60,141,188,0.9)',
+    //            borderColor: 'rgba(60,141,188,0.8)',
+    //            pointRadius: false,
+    //            pointColor: '#3b8bba',
+    //            pointStrokeColor: 'rgba(60,141,188,1)',
+    //            pointHighlightFill: '#fff',
+    //            pointHighlightStroke: 'rgba(60,141,188,1)',
+    //            data: [28, 48, 40, 19, 86, 27, 90]
+    //        },
+    //        {
+    //            label: 'Electronics',
+    //            backgroundColor: 'rgba(210, 214, 222, 1)',
+    //            borderColor: 'rgba(210, 214, 222, 1)',
+    //            pointRadius: false,
+    //            pointColor: 'rgba(210, 214, 222, 1)',
+    //            pointStrokeColor: '#c1c7d1',
+    //            pointHighlightFill: '#fff',
+    //            pointHighlightStroke: 'rgba(220,220,220,1)',
+    //            data: [65, 59, 80, 81, 56, 55, 40]
+    //        },
+    //    ]
+    //}
+
+    //var barChartData = $.extend(true, {}, areaChartData)
+    //var stackedBarChartCanvas = $('#stackedBarChart').get(0).getContext('2d')
+    //var stackedBarChartData = $.extend(true, {}, barChartData)
+
+    //var stackedBarChartOptions = {
+    //    responsive: true,
+    //    maintainAspectRatio: false,
+    //    scales: {
+    //        xAxes: [{
+    //            stacked: true,
+    //        }],
+    //        yAxes: [{
+    //            stacked: true
+    //        }]
+    //    }
+    //}
+
+    //new Chart(stackedBarChartCanvas, {
+    //    type: 'bar',
+    //    data: stackedBarChartData,
+    //    options: stackedBarChartOptions
+    //})
+
+    var temp_gender = {};
+    var departmentCounts = {};
+    $.ajax({
+        url: 'http://localhost:8082/api/Departments',
+        type: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + sessionStorage.getItem('token')
+        },
+        dataType: 'json',
+        success: function (departmentsData) {
+            // Fetch data from the "Employees" API
+            $.ajax({
+                url: 'http://localhost:8082/api/Employees',
+                type: 'GET',
+                //headers: {
+                //    'Authorization': 'Bearer ' + sessionStorage.getItem('token')
+                //},
+                dataType: 'json',
+                success: function (employeesData) {
+                     
+                    var obj = employeesData.data;
+                    debugger;
+                    // Iterasi melalui data karyawan
+                    for (var i = 0; i < obj.length; i++) {
+                        console.log(i)
+                        var employee = employeesData.data[i];
+                        var departmentId = employee.departmentId;
+                        var gender = employee.gender;
+                        var male = 0;
+                        var female = 1;
+                        if (!temp_gender.hasOwnProperty(departmentId)) {
+                            temp_gender[departmentId] = {};
+                        }
+                        // Periksa apakah departmentId sudah ada dalam departmentCounts
+                        if (!temp_gender[departmentId].hasOwnProperty(gender)) {
+                            temp_gender[departmentId][gender] = 1;
+                            
+                        } else {
+                            temp_gender[departmentId][gender]++;
+                        }
+                    }
+                    var objDept = departmentsData.data;
+                    var departmentNames = objDept.map(function (department) {
+                        return department.name;
+                    });
+                    debugger;
+                    var maleCounts = [];
+                    var femaleCounts = [];
+
+                    // Ambil jumlah karyawan laki-laki dan perempuan berdasarkan departemen
+                    for (var j = 0; j < departmentNames.length; j++) {
+                        var departmentId = objDept[j].id;
+                        if (temp_gender.hasOwnProperty(departmentId)) {
+                            var counts = temp_gender[departmentId];
+                            console.log(counts)
+                            maleCounts.push(counts[0]);
+                            femaleCounts.push(counts[1]);
+                        } else {
+                            maleCounts.push(0);
+                            femaleCounts.push(0);
+                        }
+                    }
+                    debugger;
+                    var chartData = {
+                        labels: departmentNames,
+                        datasets: [
+                            {
+                                label: 'Male',
+                                backgroundColor: 'rgba(60,141,188,0.9)',
+                                borderColor: 'rgba(60,141,188,0.8)',
+                                pointRadius: false,
+                                pointColor: '#3b8bba',
+                                pointStrokeColor: 'rgba(60,141,188,1)',
+                                pointHighlightFill: '#fff',
+                                pointHighlightStroke: 'rgba(60,141,188,1)',
+                                data: maleCounts
+                            },
+                            {
+                                label: 'Female',
+                                backgroundColor: 'rgba(255,99,132,0.9)',
+                                data: femaleCounts
+                            }
+                        ]
+                    };
+
+                    var chartOptions = {
+                        maintainAspectRatio: false,
+                        responsive: true,
+                        legend: {
+                            display: true
+                        },
+                        scales: {
+                            xAxes: [{
+                                stacked: true,
+                                gridLines: {
+                                    display: false
+                                }
+                            }],
+                            yAxes: [{
+                                stacked: true,
+                                stepSize: 1,
+                                gridLines: {
+                                    display: true
+                                }
+                            }]
+                        }
+                    };
+
+                    var chartCanvas = $('#stackedBarChart').get(0).getContext('2d');
+                    var chart = new Chart(chartCanvas, {
+                        type: 'bar',
+                        data: chartData,
+                        options: chartOptions
+                    });
+                },
+                error: function (error) {
+                    console.log(error);
+                }
+            });
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+}
+
+function barchart() {
+    var barChartCanvas = $('#barChart1').get(0).getContext('2d')
+    var temp_gender = {};
+    var departmentCounts = {};
+    $.ajax({
+        url: 'http://localhost:8082/api/Departments',
+        type: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + sessionStorage.getItem('token')
+        },
+        dataType: 'json',
+        success: function (departmentsData) {
+            // Fetch data from the "Employees" API
+            $.ajax({
+                url: 'http://localhost:8082/api/Employees',
+                type: 'GET',
+                //headers: {
+                //    'Authorization': 'Bearer ' + sessionStorage.getItem('token')
+                //},
+                dataType: 'json',
+                success: function (employeesData) {
+
+                    var obj = employeesData.data;
+                    debugger;
+                    // Iterasi melalui data karyawan
+                    for (var i = 0; i < obj.length; i++) {
+                        console.log(i)
+                        var employee = employeesData.data[i];
+                        var departmentId = employee.departmentId;
+                        var gender = employee.gender;
+                        var male = 0;
+                        var female = 1;
+                        if (!temp_gender.hasOwnProperty(departmentId)) {
+                            temp_gender[departmentId] = {};
+                        }
+                        // Periksa apakah departmentId sudah ada dalam departmentCounts
+                        if (!temp_gender[departmentId].hasOwnProperty(gender)) {
+                            temp_gender[departmentId][gender] = 1;
+
+                        } else {
+                            temp_gender[departmentId][gender]++;
+                        }
+                    }
+                    var objDept = departmentsData.data;
+                    var departmentNames = objDept.map(function (department) {
+                        return department.name;
+                    });
+                    debugger;
+                    var maleCounts = [];
+                    var femaleCounts = [];
+
+                    // Ambil jumlah karyawan laki-laki dan perempuan berdasarkan departemen
+                    for (var j = 0; j < departmentNames.length; j++) {
+                        var departmentId = objDept[j].id;
+                        if (temp_gender.hasOwnProperty(departmentId)) {
+                            var counts = temp_gender[departmentId];
+                            console.log(counts)
+                            maleCounts.push(counts[0]);
+                            femaleCounts.push(counts[1]);
+                        } else {
+                            maleCounts.push(0);
+                            femaleCounts.push(0);
+                        }
+                    }
+                    debugger;
+
+
+                    var chartData = {
+                        labels: departmentNames,
+                        datasets: [
+                            {
+                                label: 'Male',
+                                backgroundColor: 'rgba(60,141,188,0.9)',
+                                borderColor: 'rgba(60,141,188,0.8)',
+                                pointRadius: false,
+                                pointColor: '#3b8bba',
+                                pointStrokeColor: 'rgba(60,141,188,1)',
+                                pointHighlightFill: '#fff',
+                                pointHighlightStroke: 'rgba(60,141,188,1)',
+                                data: maleCounts
+                            },
+                            {
+                                label: 'Female',
+                                backgroundColor: 'rgba(255,99,132,0.9)',
+                                data: femaleCounts
+                            }
+                        ]
+                    };
+
+                    var barChartData = $.extend(true, {}, chartData)
+                    var temp0 = chartData.datasets[0]
+                    var temp1 = chartData.datasets[1]
+                    barChartData.datasets[0] = temp1
+                    barChartData.datasets[1] = temp0
+
+                    var barChartOptions = {
+                        responsive: true,
+                        stepSize: 1,
+                        maintainAspectRatio: false,
+                        datasetFill: false
+                    }
+
+
+                    new Chart(barChartCanvas, {
+                        type: 'bar',
+                        data: barChartData,
+                        options: barChartOptions
+                    })
+                },
+                error: function (error) {
+                    console.log(error);
+                }
+            });
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+}
 function GetCountDept(callback) {
     var countDept = {};
     $.ajax({
